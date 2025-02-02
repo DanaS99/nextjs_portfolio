0baf757e21bdee0a4dce5ecbd4712e0f1ace3867
Dana <danasobh46@gmail.com>
2025-02-02 19:54:07 +0200

    added sanity and queries

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,24 @@
 //import SearchForm from "@/components/SearchForm";
-//import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
-//import { STARTUPS_QUERY } from "@/sanity/lib/queries";
-//import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { auth } from "@/auth";
 import SearchForm from "@/components/SearchForm";
+
 
 export default async function Home({searchParams
  }: {
    searchParams: Promise<{ query?: string }>;
  }) {
   const query = (await searchParams).query;
-//   const params = { search: query || null };
+  //const posts = await client.fetch(STARTUPS_QUERY)
+ const params = { search: query || null };
 
 //   const session = await auth();
 
 //   console.log(session?.id);
 
-//   const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
+const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
   return (
     <>
@@ -34,12 +36,12 @@ export default async function Home({searchParams
     <SearchForm query={query} /> 
       </section>
 
-      {/* <section className="section_container">
+      <section className="section_container">
         <p className="text-30-semibold">
           {query ? `Search results for "${query}"` : "All Startups"}
-        </p> */}
+        </p>
 
-        {/* <ul className="mt-7 card_grid">
+          <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
             posts.map((post: StartupTypeCard) => (
               <StartupCard key={post?._id} post={post} />
@@ -47,10 +49,10 @@ export default async function Home({searchParams
           ) : (
             <p className="no-results">No startups found</p>
           )}
-        </ul> */}
-      {/* </section> */}
+        </ul>  
+      </section> 
 
-      {/* <SanityLive /> */}
+       <SanityLive />
     </>
   );
 }
---
+++ sanity/structure.ts
@@ -0,0 +1,11 @@
+import { StructureResolver } from 'sanity/structure'
+
+export const structure: StructureResolver = (S) => 
+  S.list()
+    .title('Content')
+    .items([
+      S.documentTypeListItem('author')
+        .title('Authors'),
+      S.documentTypeListItem('startup')
+        .title('Startup')
+    ])
